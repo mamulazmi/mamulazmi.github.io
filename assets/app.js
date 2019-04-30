959f605f8d4da5608918ee477bfd850ed0e74089
Muhammad Imamul Azmi <imamul.azmi48@gmail.com>
2019-05-01 00:42:22 +0800

    Remake Profile Pages

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    var typed = new Typed('.job', {
+        typeSpeed: 150,
+        backSpeed: 20,
+        loop: true,
+        strings: ["Web Developer.", "Python Developer.", "NodeJS Developer.", "Security Tester.", "System Administrator."]
+    });
+}) 
